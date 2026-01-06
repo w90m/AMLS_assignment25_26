b5d772f0f68cdec9ab02949e8059a2e04db9936d
w90m <83586830+w90m@users.noreply.github.com>
2026-01-06 12:43:57 +0000

    Add train_svm() function

--- train.py
+++ train.py
@@ -5,8 +5,22 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import torch
 from torchvision import transforms
+from sklearn.svm import SVC
 
 import gaussian_noise
+
+
+
+def train_svm(X_train, y_train, X_val, y_val, C_value, kernel_val):
+    svm = SVC(C=C_value, kernel=kernel_val, class_weight= "balanced")
+    svm.fit(X_train, y_train)
+
+    train_acc = svm.score(X_train, y_train)
+    val_acc   = svm.score(X_val, y_val)
+
+    return train_acc, val_acc
+
+
 
 def cnn_dataset_no_aug(train_dataset, val_dataset, test_dataset):
     basic_transform = transforms.Compose([
